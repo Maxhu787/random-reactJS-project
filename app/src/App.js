6e3c3a8d18fcbf07aef10dda916dbd01af7f3ff3
Hu Kaixiang <maxhu787@gmail.com>
2022-09-03 21:52:36 +0800

    update

--- app/src/App.js
+++ app/src/App.js
@@ -5,27 +5,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      test: 0
+      test: "test"
     };
-    this.incremental = this.incremental.bind(this);
-    this.decrement = this.decrement.bind(this);
-  }
-  incremental() {
-    this.setState({
-      test: this.state.test + 1
-    })
-  }
-  decrement() {
-    this.setState({
-      test: this.state.test - 1
-    })
+    //this.incremental = this.incremental.bind(this);
   }
   render() {
     return (
       <div>
-        <p>{this.state.test}</p>
-        <button onClick={this.incremental}>incremental</button>
-        <button onClick={this.decrement}>decrement</button>
+        <Controls />
+      </div>
+    )
+  }
+}
+class Controls extends React.Component {
+  render() {
+    const controlBtn = {
+      color: "000",
+      padding: "20px",
+      "font-size": "40px",
+      cursor: "pointer",
+    }
+    return (
+      <div style={{padding: "200px"}}>
+        <section style={{display: "inline", float: "left"}}>
+          <h3>Break Length</h3>
+          <button style={controlBtn}>↓</button>
+          <button style={controlBtn}>↑</button>
+        </section>
+        <section style={{ display: "inline", float: "right"}}>
+          <h3>Session Length</h3>
+          <button style={controlBtn}>↓</button>
+          <button style={controlBtn}>↑</button>
+        </section>
       </div>
     )
   }
