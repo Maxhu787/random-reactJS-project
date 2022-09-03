9193524feb55a92bbf639d57f8e12883cd7e91a0
Hu Kaixiang <maxhu787@gmail.com>
2022-09-03 21:40:09 +0800

    test

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 0
+    };
+    this.incremental = this.incremental.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  incremental() {
+    this.setState({
+      test: this.state.test + 1
+    })
+  }
+  decrement() {
+    this.setState({
+      test: this.state.test - 1
+    })
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.test}</p>
+        <button onClick={this.incremental}>incremental</button>
+        <button onClick={this.decrement}>decrement</button>
+      </div>
+    )
+  }
 }
-
 export default App;
